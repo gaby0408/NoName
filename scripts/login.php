7a6cd0778484d721d9da3856c6a9f33e02c5dcda
chibombo <genaro.arvizu95@gmail.com>
2018-04-01 16:00:34 -0500

    Se mejoro el login Anteriormente no detectaba si el usuario estaba bien o mal, unicamente si se pudo realizar la conexion y la peticion al servidor, ahora detecta si el usuario existe y esta bien, y si el usuario no existe o es incorrecto.

--- scripts/login.php
+++ scripts/login.php
@@ -4,16 +4,21 @@
     $username=$_POST["username"];
     $password=$_POST["password"];
     try{
-        $sql = "SELECT COUNT(*) FROM ALUMNO WHERE NOMBRE_ALUMNO = :username AND CONTRASEÑA_ALUMNO = :password";
+        $sql = "SELECT COUNT(*) FROM ALUMNO WHERE BOLETA = :username AND CONTRASEÑA_ALUMNO = :password";
         $stmt = $conn->prepare($sql);
-        if($stmt->execute(array(":username" => $username, ":password" => $passwords)))
-        {
+        $stmt->execute(array(":username" => $username, ":password" => $password));
+        $temp = $stmt->fetchColumn(0);
+        if($temp > 0){
           $message = 1;
           echo $message;
+        }else{
+            $message = 2;
+            echo $message;
         } 
+
     }catch (PDOException $ex) 
     {
-        $message = 2;
+        $message = $ex;
         echo $message;
         $conn = null;
         exit;
